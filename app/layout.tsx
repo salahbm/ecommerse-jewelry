f5100cc75f06010abe340eb92453af7344d199f6
salahbm <salahbm.001@gmail.com>
2024-01-05 20:59:41 +0900

    footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Kalam } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/shared/Navbar";
+import Footer from "@/components/shared/Footer";
 
 const spaceGrotesk = Kalam({
   subsets: ["latin"],
@@ -23,6 +24,7 @@ export default function RootLayout({
       <body className={spaceGrotesk.className}>
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   const links = [
     {
       id: 1,
-      link: "home",
+      link: "shop",
     },
     {
       id: 2,
@@ -17,22 +17,22 @@ const Navbar = () => {
     },
     {
       id: 3,
-      link: "portfolio",
+      link: "cart",
     },
+
     {
       id: 4,
-      link: "experience",
+      link: "contact",
     },
     {
       id: 5,
-      link: "contact",
+      link: "account",
     },
   ];
 
   return (
-    <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed nav">
+    <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-neutral-800 fixed nav">
       <div>
-        {/* <h1 className="text-5xl font-signature ml-2"><a className="link-underline hover:transition ease-in-out delay-150 hover:underline hover:decoration-solid" href="">Logo</a></h1> */}
         <h1 className="text-5xl font-signature ml-2">
           <a
             className="link-underline link-underline-black"
