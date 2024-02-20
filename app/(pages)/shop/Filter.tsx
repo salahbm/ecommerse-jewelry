eb010ad4caded54b500d731f45a9fbe95f789449
salahbm <salahbm.001@gmail.com>
2024-02-20 20:04:26 +0900

    filter ui

--- app/(pages)/shop/Filter.tsx
+++ app/(pages)/shop/Filter.tsx
@@ -1,32 +1,65 @@
-import React from "react";
+'use client'
+import { Slider } from '@/components/ui/slider'
+import React, { useState } from 'react'
+import { MdKeyboardArrowDown, MdKeyboardArrowUp } from 'react-icons/md'
 
 const Filter = () => {
+  const [expand, setExpand] = useState(false)
   // Sample data for each section
   const categories = [
-    "Sweater",
-    "Socks",
-    "T-Shirt",
-    "Shoes",
-    "Hoodies & Pants",
-  ];
-  const sizes = ["XL", "S", "M", "XS"];
+    'Rings',
+    'Wedding Rings',
+    'Necklaces',
+    ' Bracelets',
+    'Ear-rings',
+  ]
   const colors = [
-    "emerald",
-    "orange",
-    "rose",
-    "amber",
-    "green",
-    "pink",
-    "indigo",
-    "cyan",
-    "stone",
-    "orange",
-  ];
-  const brands = ["Amul", "RIGO", "Nyptra", "Shangri-la"];
+    'red-700',
+    'yellow-300',
+    'amber-600',
+    'green-600',
+    'blue-600',
+    'indigo-600',
+    'cyan-500',
+    'pink-500',
+  ]
+
+  const materials = ['Gold', 'Rose Gold', 'Silver', 'Titanium', 'Platinum']
+  var gemstones = [
+    'Diamond',
+    'Sapphire',
+    'Ruby',
+    'Emerald',
+    'Aquamarine',
+    'Pearl',
+    'Turquoise',
+    'Peridot',
+  ]
 
   return (
-    <div className="w-full pr-4 lg:w-1/4 lg:block">
+    <div
+      className={`relative w-full pr-4 lg:w-1/4 lg:block lg:max-h-full ${
+        expand ? 'max-h-full' : 'max-h-[250px] overflow-y-hidden'
+      }`}
+    >
+      {/* expand btn */}
+      <button
+        onClick={() => setExpand(!expand)}
+        type="button"
+        className="absolute left-1/2 transform -translate-x-1/2 lg:hidden block bottom-0 z-1 animate-bounce rounded-full border-neutral-500 border"
+      >
+        {expand ? (
+          <MdKeyboardArrowUp size={30} />
+        ) : (
+          <MdKeyboardArrowDown size={30} />
+        )}
+      </button>
       {/* Product Categories */}
+      <div className="p-4 mb-5 bg-white border border-gray-200 dark:border-gray-900 dark:bg-gray-900">
+        <h2 className="text-2xl font-bold dark:text-gray-400">Product Price</h2>
+        <div className="w-16 pb-2 mb-6 border-b border-rose-600 dark:border-gray-400"></div>
+        <Slider defaultValue={[50, 500]} max={1000} step={10} />
+      </div>
       <div className="p-4 mb-5 bg-white border border-gray-200 dark:border-gray-900 dark:bg-gray-900">
         <h2 className="text-2xl font-bold dark:text-gray-400">
           Product Categories
@@ -45,12 +78,6 @@ const Filter = () => {
             </li>
           ))}
         </ul>
-        <a
-          href="#"
-          className="text-base font-medium text-orange-500 hover:underline dark:text-orange-400"
-        >
-          View More
-        </a>
       </div>
 
       {/* Size */}
@@ -58,14 +85,7 @@ const Filter = () => {
         <h2 className="text-2xl font-bold dark:text-gray-400 ">Size</h2>
         <div className="w-16 pb-2 mb-6 border-b border-rose-600 dark:border-gray-400"></div>
         <div className="flex flex-wrap -mx-2 -mb-2">
-          {sizes.map((size, index) => (
-            <button
-              key={index}
-              className="py-1 mb-2 mr-1 border w-11 hover:border-orange-400 dark:border-gray-400 hover:text-orange-600 dark:hover:border-gray-300 dark:text-gray-400"
-            >
-              {size}
-            </button>
-          ))}
+          <Slider defaultValue={[50, 500]} max={1000} step={10} />
         </div>
       </div>
 
@@ -76,38 +96,52 @@ const Filter = () => {
         <div className="flex flex-wrap -mx-2 -mb-2">
           {colors.map((color, index) => (
             <button key={index} className="p-1 mb-2 mr-4 ">
-              <div className={`w-5 h-5 bg-${color}-400`}></div>
+              <div
+                className={`w-5 h-5 bg-${color} border border-neutral-500 hover:border-${color}`}
+              ></div>
             </button>
           ))}
         </div>
       </div>
 
-      {/* Brand */}
+      {/* Material */}
       <div className="p-4 mb-5 bg-white border border-gray-200 dark:border-gray-900 dark:bg-gray-900">
-        <h2 className="text-2xl font-bold dark:text-gray-400">Brand</h2>
+        <h2 className="text-2xl font-bold dark:text-gray-400">Material</h2>
         <div className="w-16 pb-2 mb-6 border-b border-rose-600 dark:border-gray-400"></div>
         <ul>
-          {brands.map((brand, index) => (
+          {materials.map((Material, index) => (
             <li key={index} className="mb-4">
               <label
                 htmlFor=""
                 className="flex items-center dark:text-gray-300"
               >
                 <input type="checkbox" className="w-4 h-4 mr-2" />
-                <span className="text-lg dark:text-gray-400">{brand}</span>
+                <span className="text-lg dark:text-gray-400">{Material}</span>
               </label>
             </li>
           ))}
         </ul>
-        <a
-          href="#"
-          className="text-base font-medium text-orange-500 hover:underline dark:text-orange-400"
-        >
-          View More
-        </a>
+      </div>
+      {/* Material */}
+      <div className="p-4 mb-5 bg-white border border-gray-200 dark:border-gray-900 dark:bg-gray-900">
+        <h2 className="text-2xl font-bold dark:text-gray-400">Gemstones</h2>
+        <div className="w-16 pb-2 mb-6 border-b border-rose-600 dark:border-gray-400"></div>
+        <ul>
+          {gemstones.map((stones, index) => (
+            <li key={index} className="mb-4">
+              <label
+                htmlFor=""
+                className="flex items-center dark:text-gray-300"
+              >
+                <input type="checkbox" className="w-4 h-4 mr-2" />
+                <span className="text-lg dark:text-gray-400">{stones}</span>
+              </label>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Filter;
+export default Filter
--- app/(pages)/shop/Products.tsx
+++ app/(pages)/shop/Products.tsx
@@ -14,6 +14,7 @@ import {
 } from '@/redux/shop-slicer'
 import { checkTheTimeAndFetch } from '@/lib/actions'
 import { ToastContainer, toast } from 'react-toastify'
+import Filter from './Filter'
 
 const Products = () => {
   const dispatch = useDispatch()
@@ -39,145 +40,148 @@ const Products = () => {
     fetchRecentStores()
   }, [])
   return (
-    <div className="w-full px-3 lg:w-3/4">
-      <div className="px-3 ">
-        <div className="items-center justify-between hidden px-4 py-2 mb-4 bg-gray-100 md:flex dark:bg-gray-900 ">
-          <h2 className="text-2xl dark:text-gray-400">Search Our Products</h2>
-          <div className="flex items-center justify-between">
-            <div>
-              <select
-                name=""
-                id=""
-                className="block w-40 text-base bg-gray-100 dark:text-gray-400 dark:bg-gray-900"
-              >
-                <option value="">Sort by latest</option>
-                <option value="">Sort by Price</option>
-              </select>
+    <>
+      <Filter />
+      <div className="w-full px-3 lg:w-3/4">
+        <div className="px-3 ">
+          <div className="items-center justify-between hidden px-4 py-2 mb-4 bg-gray-100 md:flex dark:bg-gray-900 ">
+            <h2 className="text-2xl dark:text-gray-400">Search Our Products</h2>
+            <div className="flex items-center justify-between">
+              <div>
+                <select
+                  name=""
+                  id=""
+                  className="block w-40 text-base bg-gray-100 dark:text-gray-400 dark:bg-gray-900"
+                >
+                  <option value="">Sort by latest</option>
+                  <option value="">Sort by Price</option>
+                </select>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      {loading ? (
-        <Loader />
-      ) : (
-        <div className="flex flex-wrap items-center">
-          {products.map((product: ProductTypes) => (
-            <div
-              key={product?._id}
-              className="w-full px-3 my-2 sm:w-1/2 md:w-1/3"
-            >
-              <div className="border border-gray-200 rounded-md dark:border-gray-800">
-                <div className="relative bg-gray-200">
-                  <Link
-                    href={`/product-detail/${product?._id}`}
-                    className="p-1"
-                  >
-                    <Image
-                      src={
-                        product.images.length > 0
-                          ? product.images[0]
-                          : '/assets/icons/no-image.svg'
-                      }
-                      alt="ProductImage"
-                      width={500}
-                      height={500}
-                      className="object-contain w-full h-56"
-                    />
-                  </Link>
-
-                  <div
-                    className={`absolute top-0 right-0 z-10 flex items-center justify-center w-12 h-12  text-center`}
-                  >
-                    <FaHeart
-                      className={`${
-                        likedItem.find(
-                          (item: ProductTypes) => item._id === product._id
-                        )
-                          ? 'text-red-500'
-                          : 'text-neutral-500'
-                      } dark:text-gray-400  text-lg`}
-                      onClick={() => {
-                        const isLiked = likedItem.find(
-                          (item: ProductTypes) => item._id === product._id
-                        )
-
-                        isLiked
-                          ? dispatch(unlikeItem(product._id))
-                          : dispatch(likedProducts(product))
-                      }}
-                    />
-                  </div>
-                </div>
-                <div className="p-4 bg-gray-50 dark:bg-gray-900">
-                  <div className="flex items-center justify-between mb-2">
-                    <h3 className="text-md lg:text-xl font-medium dark:text-gray-400 overflow-hidden overflow-ellipsis h-12">
-                      {product.title}
-                    </h3>
-
-                    <p className="text-lg ">
-                      <span className="text-lime-500 dark:text-gray-400 mx-1">
-                        ${product.newPrice}
-                      </span>
-                      <span className="text-gray-400 line-through dark:text-gray-400">
-                        {product.oldPrice}
-                      </span>
-                    </p>
-                  </div>
-
-                  <div className="flex flex-row items-center justify-between ">
-                    <p className="text-sm md:text-md font-normal text-gray-400 dark:text-gray-400 max-h-[80px] md:max-h-[110px] overflow-hidden">
-                      <span>{product.description}</span>
-                    </p>
-                    <button
-                      onClick={() => {
-                        dispatch(addToCart(product)),
-                          toast.success('Added to Cart', {
-                            position: toast.POSITION.TOP_CENTER,
-                          })
-                      }}
-                      className="flex justify-center px-4 py-2 text-amber-600 border border-amber-300 rounded-md dark:border-gray-600 dark:text-gray-400 hover:bg-amber-700 hover:text-gray-100 dark:hover:bg-gray-800 dark:hover:border-gray-900"
+        {loading ? (
+          <Loader />
+        ) : (
+          <div className="flex flex-wrap items-center">
+            {products.map((product: ProductTypes) => (
+              <div
+                key={product?._id}
+                className="w-full px-3 my-2 sm:w-1/2 md:w-1/3"
+              >
+                <div className="border border-gray-200 rounded-md dark:border-gray-800">
+                  <div className="relative bg-gray-200">
+                    <Link
+                      href={`/product-detail/${product?._id}`}
+                      className="p-1"
                     >
-                      <FaCartPlus />
-                    </button>
+                      <Image
+                        src={
+                          product.images.length > 0
+                            ? product.images[0]
+                            : '/assets/icons/no-image.svg'
+                        }
+                        alt="ProductImage"
+                        width={500}
+                        height={500}
+                        className="object-contain w-full h-56"
+                      />
+                    </Link>
+
+                    <div
+                      className={`absolute top-0 right-0 z-10 flex items-center justify-center w-12 h-12  text-center`}
+                    >
+                      <FaHeart
+                        className={`${
+                          likedItem.find(
+                            (item: ProductTypes) => item._id === product._id
+                          )
+                            ? 'text-red-500'
+                            : 'text-neutral-500'
+                        } dark:text-gray-400  text-lg`}
+                        onClick={() => {
+                          const isLiked = likedItem.find(
+                            (item: ProductTypes) => item._id === product._id
+                          )
+
+                          isLiked
+                            ? dispatch(unlikeItem(product._id))
+                            : dispatch(likedProducts(product))
+                        }}
+                      />
+                    </div>
+                  </div>
+                  <div className="p-4 bg-gray-50 dark:bg-gray-900">
+                    <div className="flex items-center justify-between mb-2">
+                      <h3 className="text-md lg:text-xl font-medium dark:text-gray-400 overflow-hidden overflow-ellipsis h-12">
+                        {product.title}
+                      </h3>
+
+                      <p className="text-lg ">
+                        <span className="text-lime-500 dark:text-gray-400 mx-1">
+                          ${product.newPrice}
+                        </span>
+                        <span className="text-gray-400 line-through dark:text-gray-400">
+                          {product.oldPrice}
+                        </span>
+                      </p>
+                    </div>
+
+                    <div className="flex flex-row items-center justify-between ">
+                      <p className="text-sm md:text-md font-normal text-gray-400 dark:text-gray-400 max-h-[80px] md:max-h-[110px] overflow-hidden">
+                        <span>{product.description}</span>
+                      </p>
+                      <button
+                        onClick={() => {
+                          dispatch(addToCart(product)),
+                            toast.success('Added to Cart', {
+                              position: toast.POSITION.TOP_CENTER,
+                            })
+                        }}
+                        className="flex justify-center px-4 py-2 text-amber-600 border border-amber-300 rounded-md dark:border-gray-600 dark:text-gray-400 hover:bg-amber-700 hover:text-gray-100 dark:hover:bg-gray-800 dark:hover:border-gray-900"
+                      >
+                        <FaCartPlus />
+                      </button>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
+        )}
+        <div className="flex justify-end mt-6">
+          <nav aria-label="page-navigation">
+            <ul className="flex list-style-none">
+              <li className="page-item disabled ">
+                <Link
+                  href="#"
+                  className="relative block pointer-events-none px-3 py-1.5 mr-3 text-base text-gray-700 transition-all duration-300  rounded-md dark:text-gray-400 hover:text-gray-100 hover:bg-amber-600"
+                >
+                  Previous
+                </Link>
+              </li>
+              <li className="page-item ">
+                <Link
+                  href="#"
+                  className="relative block px-3 py-1.5 mr-3 text-base hover:text-amber-700 transition-all duration-300 hover:bg-amber-200 dark:hover:text-gray-400 dark:hover:bg-gray-700 rounded-md text-gray-100 bg-amber-600"
+                >
+                  1
+                </Link>
+              </li>
+              <li className="page-item disabled ">
+                <Link
+                  href="#"
+                  className="relative block pointer-events-none px-3 py-1.5 mr-3 text-base text-gray-700 transition-all duration-300  rounded-md dark:text-gray-400 hover:text-gray-100 hover:bg-amber-600"
+                >
+                  Next
+                </Link>
+              </li>
+            </ul>
+          </nav>
         </div>
-      )}
-      <div className="flex justify-end mt-6">
-        <nav aria-label="page-navigation">
-          <ul className="flex list-style-none">
-            <li className="page-item disabled ">
-              <Link
-                href="#"
-                className="relative block pointer-events-none px-3 py-1.5 mr-3 text-base text-gray-700 transition-all duration-300  rounded-md dark:text-gray-400 hover:text-gray-100 hover:bg-amber-600"
-              >
-                Previous
-              </Link>
-            </li>
-            <li className="page-item ">
-              <Link
-                href="#"
-                className="relative block px-3 py-1.5 mr-3 text-base hover:text-amber-700 transition-all duration-300 hover:bg-amber-200 dark:hover:text-gray-400 dark:hover:bg-gray-700 rounded-md text-gray-100 bg-amber-600"
-              >
-                1
-              </Link>
-            </li>
-            <li className="page-item disabled ">
-              <Link
-                href="#"
-                className="relative block pointer-events-none px-3 py-1.5 mr-3 text-base text-gray-700 transition-all duration-300  rounded-md dark:text-gray-400 hover:text-gray-100 hover:bg-amber-600"
-              >
-                Next
-              </Link>
-            </li>
-          </ul>
-        </nav>
+        <ToastContainer />
       </div>
-      <ToastContainer />
-    </div>
+    </>
   )
 }
 
